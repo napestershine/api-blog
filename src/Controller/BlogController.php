0bc2710cff38de001e3f29ca0a534b389e4612f8
manu <napestershine@gmail.com>
2019-06-08 10:43:42 +0200

    added pages route

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BlogController
+ * @package App\Controller
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+
+    private const POSTS = [
+        [
+            'id' => 1,
+            'slug' => 'hello-world',
+            'title' => 'Hello World!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'another-post',
+            'title' => 'This is another post!'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'last-example',
+            'title' => 'This is the last example'
+        ]
+    ];
+
+    /**
+     * @Route("/{page}", name="blog_list", defaults={"page"=1}, requirements={"id"="\d+"})
+     */
+    public function list($page)
+    {
+        return new JsonResponse([
+            'page' => $page,
+            'data' => self::POSTS
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     */
+    public function post($id)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+        );
+
+    }
+
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug")
+     */
+    public function postBySlug($slug)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+        );
+    }
+}
