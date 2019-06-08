5dcdd8c3778b8185e28688266389d550035a5f98
manu <napestershine@gmail.com>
2019-06-08 10:05:32 +0200

    chapter 24

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default_index")
+     */
+    public function index()
+    {
+        return $this->json([
+            'action' => 'index',
+            'time' => time(),
+        ]);
+    }
+}
